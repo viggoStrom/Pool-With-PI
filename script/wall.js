498964b2716e7a780e29c9a363023b79565699e9
viggoStrom <104303404+viggoStrom@users.noreply.github.com>
2022-12-04 15:48:22 +0100

    Update wall.js

--- script/wall.js
+++ script/wall.js
@@ -2,7 +2,7 @@
 
 
 class wall {
-    constructor(x,y,w,h,color) {
+    constructor(x, y, w, h, color) {
         this.x = x;
         this.y = y;
         this.w = w;
@@ -14,8 +14,8 @@ class wall {
         ctx.beginPath();
         ctx.fillStyle = this.c;
         ctx.strokeStyle = this.c;
-        ctx.fillRect(this.x,this.y,this.w,this.h);
-        ctx.strokeRect(this.x,this.y,this.w,this.h);
+        ctx.fillRect(this.x, this.y, this.w, this.h);
+        ctx.strokeRect(this.x, this.y, this.w, this.h);
         ctx.closePath();
     }
 }
