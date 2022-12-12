720c0be41db587611e2e0ec632c96fb8b3359812
viggoStrom <104303404+viggoStrom@users.noreply.github.com>
2022-12-12 20:29:38 +0100

    reverting changes

--- script/main.js
+++ script/main.js
@@ -7,16 +7,14 @@ ctx.clearRect(0, 0, canvas.width, canvas.height)
 
 
 // declaring som variables
+canvas.perfDiv = document.getElementById("performance");
 canvas.hasTheProgramStarted = true;
-
 canvas.height = "1000"
 canvas.width = canvas.height * 1.5
-
-canvas.perfDiv = document.getElementById("performance");
+canvas.nrOfCollisions = 0
 canvas.frames = 0
 canvas.frameTime = 0
 canvas.timeFactor = 55
-
 canvas.color = {
     wall: "#999696",
     floor: "#a1a1a1",
@@ -26,40 +24,65 @@ canvas.color = {
 
 
 // declaring all of my objects
-
+const wallAndFloor = new bound(canvas)
+const smallBox = new box("Small Box", canvas, canvas.width * 10 / 15, 100, 0, 1)
+const bigBox = new box("Big Box", canvas, canvas.width * 12 / 15, 200, 100, 1000000)
+const physics = new engine(canvas, wallAndFloor, smallBox, bigBox)
 
 canvas.perfDisplay = () => {
     const frames = canvas.frames.toFixed(1);
     const fps = (canvas.frames / (performance.now() - canvas.timeAtStart) * 1000).toFixed(2);
     const frametime = (performance.now() - canvas.frameTime).toFixed(2);
     const time = (performance.now() - canvas.timeAtStart).toFixed(2);
+    const collisions = canvas.nrOfCollisions;
 
     // changes the values of the text under the canvas to reflect current stats
     canvas.perfDiv.children[0].innerText = ` frames: ${frames} `;
     canvas.perfDiv.children[1].innerText = ` fps: ${fps} `;
     canvas.perfDiv.children[2].innerText = ` frametime: ${frametime} ms `;
     canvas.perfDiv.children[3].innerText = ` time: ${time} ms `;
+    canvas.perfDiv.children[4].innerText = ` collisions: ${collisions} `;
 
     // these are used to calculate frametimes, total frames ellapsed, and fps
     canvas.frameTime = performance.now();
     canvas.frames++;
 }
 
+// draws instructions in the middle of the canvas
+canvas.instructions = () => {
+    ctx.fillStyle = "#363636";
+    ctx.fillRect(canvas.width / 20, canvas.height / 2 - 60, canvas.width * 18 / 20, 90);
+    ctx.font = "50px roboto mono";
+    ctx.fillStyle = "white";
+    ctx.fillText("Press 'space' or simply click on the screen", canvas.width / 15, canvas.height / 2);
+}
+
+
 // main frame loop
 const frame = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 
+    physics.update()
+    wallAndFloor.update()
+    smallBox.update()
+    bigBox.update()
 
     canvas.perfDisplay()
 
     window.requestAnimationFrame(frame)
 }
 
+const dimInputs = () => {
+    document.body.style.color = "gray"
+    document.querySelectorAll(".inputField").forEach(element => {
+        element.style.color = "gray"
+    });
+}
+
 // starts the game and makes sure it's not already running
 const initiate = () => {
     // simple boolean check wether it's already running or not
     if (canvas.hasTheProgramStarted) {
-
         canvas.hasTheProgramStarted = false;
 
         // needed for performance tracking
@@ -68,10 +91,11 @@ const initiate = () => {
         physics.initiate()
 
         canvas.timeFactor = document.getElementById("timeFactor").value
-        
         for (let index = 0; index < canvas.timeFactor; index++) {
             window.requestAnimationFrame(frame)
         }
+
+        dimInputs()
     }
 }
 
@@ -85,3 +109,7 @@ document.addEventListener('keydown', function (event) {
 
 // render some elements before starting so the user gets feedback that its actually working
 ctx.clearRect(0, 0, canvas.width, canvas.height)
+wallAndFloor.update()
+smallBox.update()
+bigBox.update()
+canvas.instructions()
