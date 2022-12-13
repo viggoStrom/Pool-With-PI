4b26d147a1db6bb1629f434de033dc88c7b2f516
viggoStrom <104303404+viggoStrom@users.noreply.github.com>
2022-12-13 17:14:16 +0100

    final comments

--- script/bounds.js
+++ script/bounds.js
@@ -4,24 +4,35 @@
 class bound {
     constructor(canvas) {
         this.canvas = canvas
-        this.floorHeightDivisor = 5
+
+        this.floorHeightDivisor = 5 // it's unclear but this is just the telling it to cover 1/5 of the canvas for the floor
+        this.width = this.canvas.width / 15 // it makes the wall one fifteenth of the width of the canvas 
+
         this.xPosition = 0
-        this.width = this.canvas.width / 15
     }
 
+    // draws the wall and floor
     draw = () => {
+
+        // fills wall
         ctx.fillStyle = this.canvas.color.wall
         ctx.fillRect(0, 0, this.width, this.canvas.height)
         
+        // fills floor
         ctx.fillStyle = this.canvas.color.floor
         ctx.fillRect(0, this.canvas.height - this.canvas.height / this.floorHeightDivisor, this.canvas.width, this.canvas.height / this.floorHeightDivisor)
         
+        // strokes floor
         ctx.strokeStyle = this.canvas.color.gray
         ctx.strokeRect(0, this.canvas.height - this.canvas.height / this.floorHeightDivisor, this.canvas.width, this.canvas.height / this.floorHeightDivisor)
     }
 
+    // draws the small marks in the ground. theyre mostly esthetic
     measuringMarks = () => {
+
         ctx.strokeStyle = this.canvas.color.gray
+
+        // it draws these 15 lines with a spacing of 10px and they are also 20px tall
         for (let index = 10; index < 150; index += 10) {
             ctx.beginPath()
             ctx.moveTo(index * 10, this.canvas.height - this.canvas.height / this.floorHeightDivisor)
@@ -30,6 +41,7 @@ class bound {
         }
     }
 
+    // collects all of the other methods into one so its cleaner to call them all
     update = () => {
         this.draw()
         this.measuringMarks()
